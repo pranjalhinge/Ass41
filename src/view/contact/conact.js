d1a70d679f0ab5fd9020e0c98dcd06df91a8715b
pranjalhinge <pranjalhinge4@gmail.com>
2024-10-17 10:15:23 +0530

    add img

---
+++ src/view/contact/conact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './../../component/nav-bar/navbar'
+import Footer from './../../component/footer/footer'
+import './contact.css'
+function conact() {
+  return (
+    <div>
+        <Navbar/>
+        <div className="contact">
+            </div>
+            <Footer/>
+    </div>
+  )
+}
+
+export default conact
